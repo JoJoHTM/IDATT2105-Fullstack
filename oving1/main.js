82fd4ae4779fb46e5d564e94b32b081302ef678c
JoJoHTM <jojohansenhtm@gmail.com>
2025-01-09 16:24:17 +0100

    la til random liste

--- oving1/main.js
+++ oving1/main.js
@@ -5,7 +5,8 @@ function updateCounterDiv() {
         counterDiv.id = 'counterDiv';
         counterDiv.className = 'paragrafer';
         counterDiv.innerHTML = 'Button pressed: 0 times';
-        document.getElementById('innhold').appendChild(counterDiv);
+        let innhold = document.getElementById('innhold');
+        innhold.insertBefore(counterDiv, randomArrayDiv);
 
         cerateResetButton();
     }
@@ -33,4 +34,21 @@ function removeCounterDiv() {
     let resetButton = document.getElementById('resetButton');
     counterDiv.remove();
     resetButton.remove();
+}
+
+function randomList() {
+    let arr = ['Ole', 'Dole', 'Doffen', 'Donald', 'Daisy', 'Pluto', 'Mikke', 'Langbein', 'Skrue', 'Rip', 'Rap', 'Rup'];
+    let randomArrayDiv = document.createElement('div');
+    randomArrayDiv.id = 'randomArrayDiv';
+    randomArrayDiv.className = 'paragrafer';
+    document.getElementById('innhold').appendChild(randomArrayDiv);
+
+    let ul = document.createElement('ul');
+    randomArrayDiv.appendChild(ul);
+
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = arr[i];
+        ul.appendChild(li);
+    }
 }
