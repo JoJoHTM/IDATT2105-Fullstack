badb44f37479baa7d4bad434872fb8efa6c7b387
jonasgjo <jonasgjo@stud.ntnu.no>
2025-01-14 14:53:21 +0100

    added component

---
+++ oving2/src/components/Buttons.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="button">
+        1
+    </div>
+</template>
+
+<style scoped>
+
+    #button {
+        background-color: rgb(81, 76, 76);
+        border-radius: 10px;
+        color: white;
+        grid-column: 1 / span 2;
+        grid-row: 2 / span 3;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40%;
+        height: 40%;
+    }
+
+</style>
---
+++ oving2/src/components/ResultItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="result">
+        55555
+    </div>
+</template>
+<style scoped>
+    #result {
+        background-color: rgb(81, 76, 76);
+        border-radius: 10px;
+        color: white;
+        grid-column: 1 / span 4;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
