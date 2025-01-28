73a1522fe4299f30f4879d4da0c06a8731aa1cc7
jonasgjo <jonasgjo@stud.ntnu.no>
2025-01-28 13:28:11 +0100

    style form

--- oving3/src/components/Feedback.vue
+++ oving3/src/components/Feedback.vue
@@ -1,12 +1,12 @@
 <template>
     <h1>Feedback</h1>
-        <form action="/">
+        <form @submit.prevent="sendFeedback">
             <label for="navn">Navn:</label>
             <input type="text" placeholder="navn" name="navn" id="navn"></input>
             <label for="email">E-post:</label>
             <input type="email" placeholder="email" id="email" name="email"></input>
-            <label for="tilbakemelding">Tilbakemelding:</label>
-            <input type="text" placeholder="skriv din tilbakemelding her" name="tilbakemelding" id="tilbakemelding"></input>
+            <label for="tilbakemelding" id="tilbakemeldingtekst">Tilbakemelding:</label>
+            <textarea placeholder="skriv din tilbakemelding her" name="tilbakemelding" id="tilbakemelding"></textarea>
             <input type="submit" value="Send" id="submit"></input>
         </form>
 </template>
@@ -14,17 +14,56 @@
 <style scoped>
 form {
     display: grid;
+    grid-template-areas: 
+        'navnlabel navn navn'
+        'emaillabel email email'
+        'tilbakemeldinglabel tilbakemelding tilbakemelding'
+        'send send send';
+
     grid-template-columns: auto auto;
-    width: 50%;
+    width: 40%;
+    height: 60vh;
     margin: 0 auto;
     padding: 20px;
     border: 1px solid black;
     border-radius: 5px;
 }
 
+label {
+    font-size: 19px;
+}
+
+#navn:focus,
+#email:focus {
+    outline: none;
+    border-bottom: 2px solid black;
+}
+
+#navn {
+    grid-area: navn;
+    height: fit-content;
+    border: none;
+    border-bottom: 2px solid gray;
+}
+
+#email {
+    grid-area: email;
+    height: fit-content;
+    border: none;
+    border-bottom: 2px solid gray;
+}
+
+#tilbakemelding {
+    grid-area: tilbakemelding;
+    resize: none;
+}
+
 #submit {
-    grid-column: 1 / span 2;
-    margin-top: 10px;
-    width: fit-content;
+    grid-area: send;
+    width: 100px;
+    height: 30px;
+    border-radius: 15px;
+    justify-self: center;
+    align-self: center;
 }
 </style>
